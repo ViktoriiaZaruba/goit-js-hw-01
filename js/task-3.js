918014702f7dee1c16c61795c8b4844c6d4df32e
ViktoriiaZaruba <vikadrakon72@gmail.com>
2024-10-12 11:16:11 +0300

    add-4

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     content = Number.parseFloat(content);
     padding = Number.parseFloat(padding);
     border = Number.parseFloat(border);
-    return `${content + 2 * padding + 2 * border}`
+    return content + 2 * padding + 2 * border
 };
 
 
